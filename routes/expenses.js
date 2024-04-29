98d28f5f1c665c4c589314b3ecf3a4b963c44be6
Naveena Kandasamy Rajkumar <naveraj07@gmail.com>
2024-04-29 13:17:56 -0500

    creating and linking add income/expense page

--- routes/expenses.js
+++ routes/expenses.js
@@ -8,4 +8,9 @@ router.get('/', async (req, res, next) => {
   res.render('view-all-expenses',{ data: incomeExpenseData});
 });
 
+/* GET Create Income Expense Page */
+router.get('/create-expense', (req, res, next) => {
+  res.render('add-income-or-expense',{title: 'Create a New Income / Expense', buttonText: 'Add Income/Expense', actionURL: ''});
+});
+
 module.exports = router;
