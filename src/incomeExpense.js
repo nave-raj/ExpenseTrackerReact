c94e2d08d00a3a63f268a1d81b01fe1301fc9f3f
Naveena Kandasamy Rajkumar <naveraj07@gmail.com>
2024-04-28 21:44:36 -0500

    adding incomeExpense Object Model

---
+++ src/incomeExpense.js
@@ -0,0 +1,11 @@
+class IncomeExpense {
+    constructor(id, type, category, description, amount) {
+      this.id = id;
+      this.type = type;
+      this.category = category;
+      this.description = description;
+      this.amount = amount;
+    }
+  }
+  
+  module.exports = IncomeExpense;
