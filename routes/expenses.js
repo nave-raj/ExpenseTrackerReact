cfd986e0618afd8b495f8102b614d81d8141be2c
Naveena Kandasamy Rajkumar <naveraj07@gmail.com>
2024-04-29 15:46:29 -0500

    renaming add incomeexpense page

--- routes/expenses.js
+++ routes/expenses.js
@@ -5,12 +5,24 @@ const expenseRepository = require('../src/repository');
 /* GET all movie listing */
 router.get('/', async (req, res, next) => {
   const incomeExpenseData = await expenseRepository.findAll();
+  console.log(incomeExpenseData);
   res.render('view-all-expenses',{ data: incomeExpenseData});
 });
 
 /* GET Create Income Expense Page */
 router.get('/create-expense', (req, res, next) => {
-  res.render('add-income-or-expense',{title: 'Create a New Income / Expense', buttonText: 'Add Income/Expense', actionURL: ''});
+  res.render('add-edit-incomeexpense',{title: 'Create a New Income / Expense', buttonText: 'Add Income/Expense', actionURL: '/create'});
+});
+
+/* GET individual income or expense by id */
+router.get('/:id', async (req, res, next) => {
+  const incomeExpenseById = await expenseRepository.findById(req.params.id);
+  console.log(incomeExpenseById);
+  if(incomeExpenseById){
+  res.render('view-incomeexpense', { incomeExpense: incomeExpenseById});
+  } else {
+    res.redirect('/expenses');
+  }
 });
 
 module.exports = router;
